4080b867fd286d4124c04b0089832ea8ce0c0a37
Mehedi Islam Remon <islammehedi60@gmail.com>
2026-03-02 13:01:51 +0600

    chore: remove obsolete device-specific node project directories
    - Deleted multiple device-specific node projects (Carrier, False Node, Gree, Midea, Samsung, York)
    - Removed associated include/, src/, lib/, .vscode/, platformio.ini, and test/ directories
    - Eliminated redundant headers, source files (IR, mesh, sensor, debug, etc.), and PlatformIO configurations
    - Cleanup consolidates codebase and removes duplicated/obsolete projects

--- Carrier - 01/swapno_node_program_carrier_1/include/data_type_conversion.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "app_config.h"
-
-int hexStringToByteArray(String hexStr, uint8_t *byteArray);
-uint8_t hexCharToByte(char hex);
-String byteArrayToHexString(uint8_t *byteArray, int length);
--- York - 02/swapno_node_program_york_2/include/request_handler.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "app_config.h"
-
-uint8_t modbusRequestHandler(uint8_t *requestBuffer, uint8_t *responseBuffer);
